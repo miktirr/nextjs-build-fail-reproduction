ea28315634d19c1b7736f63517378c8b4b4ac581
miktirr <mt@mtirronen.org>
2025-03-08 16:30:41 +0200

    Add vanilla-extract

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import { container } from "./page.css";
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null;
+  return <div className={container}>I am broken.</div>;
 }
